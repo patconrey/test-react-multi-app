5c56cbc891ac3921da50a99322df52a3bc3bc846
Patrick Conrey <patrickconrey@gmail.com>
2019-06-28 17:21:02 -0500

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require("webpack")
 const dotenv = require("dotenv")
 
 module.exports = () => {
-    const env = dotenv.config({ path: "./.env.development" }).parsed
+    const env = dotenv.config().parsed
     const envKeys = Object.keys(env).reduce((prev, next) => {
         prev[`process.env.${next}`] = JSON.stringify(env[next])
         return prev
