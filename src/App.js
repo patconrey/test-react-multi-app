cf7472b6a8104998a9ce04c92b745fe1c9f5a167
Patrick Conrey <patrickconrey@gmail.com>
2019-06-28 14:01:03 -0500

    updated styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component} from "react"
 import { hot } from "react-hot-loader"
-import "./styles/App.scss"
+import "./styles/layout/_layout.scss"
 
 class App extends Component{
   render(){
     return(
-      <div className="App">
+      <div className="container">
         <h1> Hello, World!</h1>
       </div>
     )
